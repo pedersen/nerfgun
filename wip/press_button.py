a5f343215a3a9d32fc32fb5aaf749ba6c8602e43
Michael J. Pedersen <m.pedersen@icelus.org>
2021-05-03 14:52:06 -0400

    Move testing code to WIP folder

---
+++ wip/press_button.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+pinnum = 40
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(pinnum, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+while True: # Run forever
+    if GPIO.input(pinnum) == GPIO.HIGH:
+        print("Button was pushed!")
